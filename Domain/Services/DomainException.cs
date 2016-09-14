c12e88e649aaf72d9b83dbd004bfa068efd73024
wowhy <wowhy@outlook.com>
2016-09-15 00:25:11 +0800

    添加DomainService

---
+++ Domain/Services/DomainException.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Repositories
+{
+    using System;
+    using System.Runtime.Serialization;
+    using Domain.Core;
+
+    public class DomainException : CoreException
+    {
+        public DomainException() { }
+
+        public DomainException(string message)
+            : base(message)
+        { }
+
+        public DomainException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        public DomainException(string format, params object[] args)
+            : base(string.Format(format, args))
+        { }
+
+        protected DomainException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
+    }
+}
