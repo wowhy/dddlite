547eea07e0274ba16ba512d013db2dc14b6223b0
wowhy <wowhy@outlook.com>
2017-04-23 10:19:11 +0800

    删除无用项目

--- src/DDDLite.WebApi.Nancy/ApiBootstrapper.cs
+++
@@ -1,8 +0,0 @@
-using Nancy;
-
-namespace DDDLite.WebApi.Nancy
-{
-    public abstract class ApiBootstrapper : DefaultNancyBootstrapper
-    {
-    }
-}
--- test/TestWebApi.Nancy/Commands/ChangePasswordCommand.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace TestWebApi.Commands
-{
-    public class ChangePasswordCommand
-    {
-    }
-}
