5716aa9a67c893b55588e1117f8782ad0cc45fc9
wowhy <wowhy@outlook.com>
2017-08-14 10:09:55 +0800

    添加TODO

--- src/DDDLite.WebApi/Internal/Parser/FilterParser.cs
+++ src/DDDLite.WebApi/Internal/Parser/FilterParser.cs
@@ -17,6 +17,9 @@ namespace DDDLite.WebApi.Internal.Parser
         {
             try
             {
+                // TODO: 使用自定义语法解析
+                // 动态lambda在使用中比较复杂难以学习 
+
                 var lambda = DynamicExpressionParser.ParseLambda(typeof(TAggregateRoot), typeof(bool), filter);
                 var filterSpecification = Specification<TAggregateRoot>.Eval(lambda as Expression<Func<TAggregateRoot, bool>>);
                 return filterSpecification;
