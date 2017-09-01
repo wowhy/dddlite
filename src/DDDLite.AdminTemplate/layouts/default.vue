8f0aa7528b952fb5a34226d4724f64df6eff72b8
wowhy <wowhy@outlook.com>
2017-09-01 20:54:30 +0800

    添加admin template项目

---
+++ src/DDDLite.AdminTemplate/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  flex: 1;
+  height: 100%;
+}
+</style>
