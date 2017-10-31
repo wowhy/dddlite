6898795419e2c4a27e4a2cac5c2b1e9eae371c98
wowhy <wowhy@outlook.com>
2017-10-31 13:03:25 +0800

    调整构建脚本

--- build.ps1
+++ build.ps1
@@ -4,6 +4,6 @@ dotnet build .\src\DDDLite -c Release
 dotnet build .\src\DDDLite.Repositories.EntityFramework -c Release
 dotnet build .\src\DDDLite.WebApi -c Release
 
-dotnet pack .\src\DDDLite -c Release -o .\artifacts
-dotnet pack .\src\DDDLite.Repositories.EntityFramework -c Release -o .\artifacts
-dotnet pack .\src\DDDLite.WebApi -c Release -o .\artifacts
+dotnet pack .\src\DDDLite -c Release -o ..\..\artifacts
+dotnet pack .\src\DDDLite.Repositories.EntityFramework -c Release -o ..\..\artifacts
+dotnet pack .\src\DDDLite.WebApi -c Release -o ..\..\artifacts
