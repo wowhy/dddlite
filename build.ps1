24c5d7b1476b042eeb3d3bb4e18b7f1fd15c59bf
wowhy <wowhy@outlook.com>
2017-10-31 16:24:32 +0800

    调整构建

--- build.ps1
+++ build.ps1
@@ -4,8 +4,8 @@ dotnet build .\src\DDDLite -c Release
 dotnet build .\src\DDDLite.Repositories.EntityFramework -c Release
 dotnet build .\src\DDDLite.WebApi -c Release
 
-dotnet pack .\src\DDDLite -c Release -o ..\..\artifacts --version-suffix=$env:APPVEYOR_BUILD_NUMBER
-dotnet pack .\src\DDDLite.Repositories.EntityFramework -c Release -o ..\..\artifacts --version-suffix=$env:APPVEYOR_BUILD_NUMBER
-dotnet pack .\src\DDDLite.WebApi -c Release -o ..\..\artifacts --version-suffix=$env:APPVEYOR_BUILD_NUMBER
+dotnet pack .\src\DDDLite -c Release -o ..\..\artifacts --version-suffix=preview-$env:APPVEYOR_BUILD_NUMBER
+dotnet pack .\src\DDDLite.Repositories.EntityFramework -c Release -o ..\..\artifacts --version-suffix=preview-$env:APPVEYOR_BUILD_NUMBER
+dotnet pack .\src\DDDLite.WebApi -c Release -o ..\..\artifacts --version-suffix=preview-$env:APPVEYOR_BUILD_NUMBER
 
 dotnet nuget push .\artifacts\*.nupkg -k b9911133-6c64-4c93-8954-042141e984a3 --source https://www.myget.org/F/wowhy/api/v2/package
