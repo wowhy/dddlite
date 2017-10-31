7d70a6348ceef8b9c6bea4b6d251898515d32d27
wowhy <wowhy@outlook.com>
2017-10-31 12:40:06 +0800

    添加build脚本

---
+++ build.ps1
@@ -0,0 +1,9 @@
+dotnet restore
+
+dotnet build .\src\DDDLite -c Release
+dotnet build .\src\DDDLite.Repositories.EntityFramework -c Release
+dotnet build .\src\DDDLite.WebApi -c Release
+
+dotnet pack .\src\DDDLite -c Release -o .\artifacts
+dotnet pack .\src\DDDLite.Repositories.EntityFramework -c Release -o .\artifacts
+dotnet pack .\src\DDDLite.WebApi -c Release -o .\artifacts
