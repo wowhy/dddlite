f8632c6ba1f4d1bc58248c6262f4f61576319509
wowhy <wowhy@outlook.com>
2017-11-26 11:09:52 +0800

    修改代码

--- src/DDDLite.Repositories.EntityFramework/UnitOfWorkDbContext.cs
+++ src/DDDLite.Repositories.EntityFramework/UnitOfWorkDbContext.cs
@@ -1,43 +1,50 @@
 namespace DDDLite.Repositories.EntityFramework
 {
-    using System.Threading;
-    using System.Threading.Tasks;
-    using DDDLite.Exception;
-    using DDDLite.Repositories;
+  using System.Linq;
+  using System.Threading;
+  using System.Threading.Tasks;
+  using DDDLite.Exception;
+  using DDDLite.Repositories;
 
-    using Microsoft.EntityFrameworkCore;
+  using Microsoft.EntityFrameworkCore;
 
-    public abstract class UnitOfWorkDbContext : DbContext, IUnitOfWork
+  public abstract class UnitOfWorkDbContext : DbContext, IUnitOfWork
+  {
+    public UnitOfWorkDbContext(DbContextOptions options) : base(options) { }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
     {
-        public UnitOfWorkDbContext(DbContextOptions options) : base(options) { }
+      this.EnsureLogicalDeleteChanging();
+      this.EnsureConcurrencyCheck();
+      this.EnsureTrackableChanging();
 
-        public override int SaveChanges(bool acceptAllChangesOnSuccess) 
-        {
-            this.EnsureLogicalDeleteChanging();
-            this.EnsureConcurrencyCheck();
-            this.EnsureTrackableChanging();
-
-            return base.SaveChanges(acceptAllChangesOnSuccess);
-        }
-
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            this.EnsureLogicalDeleteChanging();
-            this.EnsureConcurrencyCheck();
-            this.EnsureTrackableChanging();
-
-            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-        }
-
-        public async Task CommitAsync(CancellationToken cancellationToken)
-        {
-            try {
-                await this.SaveChangesAsync();
-            }
-            catch(DbUpdateConcurrencyException ex)
-            {
-                throw new ConcurrencyException(ex);
-            }
-        }
+      return base.SaveChanges(acceptAllChangesOnSuccess);
     }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+    {
+      this.EnsureLogicalDeleteChanging();
+      this.EnsureConcurrencyCheck();
+      this.EnsureTrackableChanging();
+
+      return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    public async Task CommitAsync(CancellationToken cancellationToken)
+    {
+      try
+      {
+        await this.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException ex)
+      {
+        foreach (var item in this.ChangeTracker.Entries())
+        {
+          item.State = EntityState.Detached;
+        }
+
+        throw new ConcurrencyException(ex);
+      }
+    }
+  }
 }
